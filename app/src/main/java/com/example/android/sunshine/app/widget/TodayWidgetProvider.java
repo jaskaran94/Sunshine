02f697a22cb30330ab343d44cf3edfaab2ea25ea
JASKARAN <jaskaran9356@gmail.com>
2016-05-17 16:32:15 +0530

    Added 1X1 widget problem in previous commit Settings Activity crashes on devices that dont have Google Play Services Installed

---
+++ app/src/main/java/com/example/android/sunshine/app/widget/TodayWidgetProvider.java
@@ -0,0 +1,37 @@
+package com.example.android.sunshine.app.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
+
+/**
+ * Created by pc on 5/17/2016.
+ */
+
+/* Delegates widget updating to {@link TodayWidgetIntentService} to ensure that
+* data retrieval is done on background thread
+*/
+
+public class TodayWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
+        context.startService(new Intent(context, TodayWidgetIntentService.class));
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        context.startService(new Intent(context, TodayWidgetIntentService.class));
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (SunshineSyncAdapter.ACTION_DATA_UPDATED.equals(intent.getAction())){
+            context.startService(new Intent(context, TodayWidgetIntentService.class));
+        }
+    }
+}
